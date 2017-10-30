c7d736098a0b29afb774a12c8f54905fe6b14898
pampas93 <abhijit.93@hotmail.com>
2017-10-29 22:44:48 -0400

    Selecting Object with UI + shader change

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -7,9 +7,10 @@ public class Manager : MonoBehaviour {
 
     [SerializeField]
     InputField username;
-
     [SerializeField]
     InputField password;
+    [SerializeField]
+    Text errorLogin;
 
     [SerializeField]
     Canvas LoginCanvas;
@@ -21,10 +22,14 @@ public class Manager : MonoBehaviour {
     GameObject sphere;
 
     private GameObject currentObject = null;
+    Shader selected_Shader;
 
     bool userLoggedIn = false;
 
     public static Manager instance;
+
+    private GameObject selectedObject = null;
+    private GameObject previous_selectedObject = null;
 
     private void Awake()
     {
@@ -41,17 +46,25 @@ public class Manager : MonoBehaviour {
                 cube.SetActive(true);
                 sphere.SetActive(false);
                 currentObject = cube;
+                selectedObject = null;
+                sphere.GetComponent<Renderer>().material.shader = Shader.Find("Standard");
             }
-                
-
+            
             if (GUI.Button(new Rect(10, 60, 70, 35), "Sphere"))
             {
                 Debug.Log("Show a sphere");
                 cube.SetActive(false);
                 sphere.SetActive(true);
                 currentObject = sphere;
+                selectedObject = null;
+                cube.GetComponent<Renderer>().material.shader = Shader.Find("Standard");
             }
-                
+            string temp = "Currently Selected : ";
+            if (selectedObject == null)
+                temp = temp + "Null";
+            else
+                temp = temp + selectedObject.name;
+            GUI.Label(new Rect(600, 10, 250, 20), temp);
         }
         
     }
@@ -60,22 +73,63 @@ public class Manager : MonoBehaviour {
     void Start () {
         cube.SetActive(false);
         sphere.SetActive(false);
-	}
+
+        selected_Shader = Shader.Find("Self-Illumin/Diffuse");
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(r, out hit))
+            {
+                if(hit.collider.tag == "PrimitiveObject")
+                {
+
+                    if (selectedObject == null)
+                    {
+                        previous_selectedObject = null;
+                        selectedObject = hit.collider.gameObject;
+                        Debug.Log(selectedObject.name + " is selected");
+                        selectedObject.GetComponent<Renderer>().material.shader = selected_Shader;
+                    }
+                    else
+                    {
+                        selectedObject.GetComponent<Renderer>().material.shader = Shader.Find("Standard");
+                        previous_selectedObject = selectedObject;
+                        selectedObject = null;
+                        Debug.Log(previous_selectedObject.name + " is unselected");
+                    }
+                    
+                }
+               
+            }
+        }
+
+    }
+
     public void loginOnClick()
     {
         string user = username.text;
         string pass = password.text;
 
-        if(user == "abhijit" && pass == "1993")
+        if(user == "abhijit" && pass == "pass")
         {
             LoginCanvas.enabled = false;
             userLoggedIn = true;
+        }
+        else
+        {
+            errorLogin.text = "Wrong Credentials";
         }
     }
 }
